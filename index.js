01c03f221bcaa89bbc9997a529f129ff9c28d62f
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-09-11 17:13:06 +0530

    Adding

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-const btn = document.querySelector('.btn');
-const form = document.getElementById('my-form');
-btn.addEventListener('click', (e) => {
-    btn.style.background = 'red';
-});
-
-btn.addEventListener('mouseover', (event) => {
-    event.preventDefault();
-    btn.style.background = 'blue';
-});
-btn.addEventListener('mouseout', (event) => {
-    btn.style.background = 'green';
-});
-
-
-
-const myForm1 = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-
-
-myForm1.addEventListener('submit',onSubmit);
-
-function onSubmit (e) {
-    e.preventDefault();
-    console.log(nameInput.value);
-    if (nameInput.value === '' || emailInput.value === '') {
-    alert('Please Enter fields');
-    } else {
-    alert('Success');
-    }
-}
-
-console.log("Just Creating");
+//var headTitle = document.getElementById("header-title");
+//headTitle.style.borderBottom = 'solid 3px #000' ;
+var seletingAdd = document.getElementsByClassName('title');
+console.log(seletingAdd[0]); 
+seletingAdd[0].textContent = "Changing text Content" ;
+seletingAdd[0].style.fontWeight = 'bold'  ;
+seletingAdd[0].style.color = 'green' ; 
