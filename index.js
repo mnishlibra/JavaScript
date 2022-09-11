d25dd96e0efc8f72807379589cf7989dce364d41
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-09-11 22:19:38 +0530

    Assignment 6 Complete

--- index.js
+++ index.js
@@ -9,13 +9,42 @@
 // 3rd Element in the list make the background green
 // Get element by tag Name and store in a variable 
 
-var li = document.getElementsByTagName('li');
-li[2].style.backgroundColor = 'green' ; 
+//var li = document.getElementsByTagName('li');
+//li[2].style.backgroundColor = 'green' ; 
 
 
 // Make all the elements in the list have bold color font 
 // For selecint all the element from 1 to end we have to use for loop 
 
-for (let i = 0 ; i < li.length ; i++) {
-    li[i].style.fontWeight = 'bold' ; 
-}
+//for (let i = 0 ; i < li.length ; i++) {
+//    li[i].style.fontWeight = 'bold' ; 
+//}
+
+
+// Getting by Class Name 
+// var list1 = document.getElementsByClassName("list-group");
+// var list2 = list1.items.lastElementChild ;
+// list2.style.color = 'green' ; 
+
+// Getting by Tag Name 
+
+//var List3 = document.getElementsByTagName('item5')
+// List3.style.color = 'green' ; 
+
+// // // var queery1 = document.querySelector(".list-group-item:nth-child(2)") ; 
+// // // var queery2 = document.querySelector(".list-group-item:nth-child(3)") ; 
+// // // queery1.style.backgroundColor = 'green' ; 
+// // // console.log(queery1);
+
+// // queery2.style.color = 'white' ;
+// //queery2.style.display = 'none' ;
+
+// var titles = document.querySelectorAll('.title') ; 
+// console.log(titles) ; 
+// titles[1].textContent = "SUPERBBBB"
+
+
+// for (let i = 0; i<odd.length; i++) {
+//     odd[i].style.backgroundColor = 'green' ; 
+// }
+
