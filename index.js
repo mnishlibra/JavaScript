6d2e543173f5d4a6b14da2a423f4edd1a3d46f80
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-09-11 19:38:54 +0530

    Assignment  5 Done

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-//var headTitle = document.getElementById("header-title");
-//headTitle.style.borderBottom = 'solid 3px #000' ;
-var seletingAdd = document.getElementsByClassName('title');
-console.log(seletingAdd[0]); 
-seletingAdd[0].textContent = "Changing text Content" ;
-seletingAdd[0].style.fontWeight = 'bold'  ;
-seletingAdd[0].style.color = 'green' ; 
+// var headTitle = document.getElementById("header-title");
+// headTitle.style.borderBottom = 'solid 3px #000' ;
+// var seletingAdd = document.getElementsByClassName('title');
+// console.log(seletingAdd[0]); 
+// seletingAdd[0].textContent = "Changing text Content" ;
+// seletingAdd[0].style.fontWeight = 'bold'  ;
+// seletingAdd[0].style.color = 'green' ; 
+
+// 3rd Element in the list make the background green
+// Get element by tag Name and store in a variable 
+
+var li = document.getElementsByTagName('li');
+li[2].style.backgroundColor = 'green' ; 
+
+
+// Make all the elements in the list have bold color font 
+// For selecint all the element from 1 to end we have to use for loop 
+
+for (let i = 0 ; i < li.length ; i++) {
+    li[i].style.fontWeight = 'bold' ; 
+}
