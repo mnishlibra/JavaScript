ddae88f6591cca3d8d0b976e9e9fab2674243f50
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-10-20 13:58:02 +0530

    Commiting Project

---
+++ Advance JS/practice.js
@@ -0,0 +1,10 @@
+function fun3() {
+	var a =10;
+	{
+		Let b=100
+	}
+	console.log(b)
+
+}
+
+fun3()
--- ExpenseTrackerProject/main.js
+++ ExpenseTrackerProject/main.js
@@ -1,30 +1,89 @@
-//fetching the values from the input form
-var form = document.getElementById("addingexpensebutton");
-var expenseAmount = document.getElementById("expamt");
-var discrip = document.getElementById("description");
-var category = document.getElementById("catego");
-//var li = document.createElement('li');
-var li = document.getElementsByClassName("list-group-item");
-//Adding event listener whenever the user clicks on the add expense button
-form.addEventListener('click' , addItem)  ; 
-//Add Item Function 
-function addItem(e) {
-    e.preventDefault(); 
-    //Getting the input value 
-    var expenseAmount = document.getElementById("expamt").value;
-    var discrip = document.getElementById("description").value;
-    var category = document.getElementById("catego").value;
-    console.log(expenseAmount);
-    console.log(discrip);
-    console.log(category);
-    // Create new li element
-    var li = document.createElement('li');
-    // Add class
-    li.className = 'list-group-item';
-    // Add text node with input value
-    li.appendChild(document.createTextNode(expenseAmount));
-    li.appendChild(document.createTextNode(discrip)); 
+function saveToLocalStorage(event) {
+    event.preventDefault();
+    const name = event.target.username.value;
+    const email = event.target.ChoosingCat.value;
+    const phonenumber = event.target.phonenumber.value;
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('phonenumber', phonenumber)
+    const obj = {
+        name,
+        email,
+        phonenumber
+    }
+    localStorage.setItem(obj.email, JSON.stringify(obj))
+    showNewUserOnScreen(obj)
+    axios.post('https://crudcrud.com/api/5f2f24bbe34f4ff5b00563c1a3cc3bca/expense',obj)
+    .then((response) => {
+        for(var i=0; i<response.data.length; i++){
+            showNewUserOnScreen(response.data[i]);
+        }
+        console.log(response)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 }
-//Remove Item 
 
-//Edit Item
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get('https://crudcrud.com/api/5f2f24bbe34f4ff5b00563c1a3cc3bca/expense')
+    .then((response) => {
+        console.log(response);
+        for(var i=0 ; i<response.data.length; i++)
+        {
+            showNewUserOnScreen((response.data[i]))
+        }
+    })  
+    .catch((err) => {
+        console.log(err);
+        console.log('Something Went Wrong')
+    })
+
+})
+
+function showNewUserOnScreen(user){
+    document.getElementById('email').value = '';
+    document.getElementById('username').value = '';
+    document.getElementById('phonenumber').value ='';
+    // console.log(localStorage.getItem(user.emailId))
+    // if(localStorage.getItem(user.email) !== null){
+    //     removeUserFromScreen(user.email)
+    // }
+
+    const parentNode = document.getElementById('listOfUsers');
+    const childHTML = `<li id=${user._id}> ${user.name} - ${user.email} - ${user.phonenumber}
+                            <button onclick=deleteUser('${user._id}')> Delete User </button>
+                            <button onclick=editUserDetails('${user.name}','${user.email}','${user.phonenumber}','${user._id}')>Edit User </button>
+                         </li>`
+
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
+}
+
+//Edit User
+
+function editUserDetails(name, email,phonenumber,userId){
+    document.getElementById('email').value = email;
+    document.getElementById('username').value = name;
+    document.getElementById('phonenumber').value =phonenumber;
+    deleteUser(userId)
+ }
+// deleteUser('abc@gmail.com')
+
+function deleteUser(userId){
+    // localStorage.removeItem(ChoosingCat);
+    axios.delete(`https://crudcrud.com/api/5f2f24bbe34f4ff5b00563c1a3cc3bca/expense/${userId}`)
+    .then((response) => {
+        removeUserFromScreen(userId)
+    })
+    .catch ((err) => {
+        err => console.log(err)
+    })
+}
+
+function removeUserFromScreen(userId){
+    const parentNode = document.getElementById('listOfUsers');
+    const childNodeToBeDeleted = document.getElementById(userId);
+    if(childNodeToBeDeleted) {
+        parentNode.removeChild(childNodeToBeDeleted)
+    }
+}
