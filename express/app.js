b14bfd4938452f378ab7e94dd43fcea7e0c80cc2
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-10-31 15:13:07 +0530

    Updating

--- express/app.js
+++ express/app.js
@@ -3,6 +3,10 @@ const app = express();
 
 const port = process.env.port || 3000;
 
+app.use('/add-product' , (req,res,next) ==> {
+    res.send('<form action=/><input></input><button></button></form')
+})
+
 app.use((req,res,next) => {
     console.log('This is the first Middleware');
     next();
