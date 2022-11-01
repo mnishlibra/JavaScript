f457e68bb5ef08e80c2975ad34a1214b77463255
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-11-01 15:04:07 +0530

    serving HTML , CSS

--- express/app.js
+++ express/app.js
@@ -1,17 +1,26 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(express.static(path.join(__dirname,'public')));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contacts');
 
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/admin' , adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);    
+app.use(contactRoutes);
 
-app.use((req , res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
-}) ; 
+app.use('/success', (req,res,next) => {
+    res.send('<html><body><script>alert("Form successfuly filled")</script></body></html>')
+})
 
-app.listen(3000) ;
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,15 +1,20 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
-const bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({extended: false}));
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="title of Product"><input type="text" name="size" placeholder="size of Product"><button type="submit">Add Product</button></form>');
-  });
-
-router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-module.exports = router ;
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
