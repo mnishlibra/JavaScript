60260b3375afd4f37214eb732d2a8c9709d2f5da
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-11-02 16:38:37 +0530

    NodeJs Project Done

--- express/app.js
+++ express/app.js
@@ -1,26 +1,25 @@
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+const bodyParser = require('body-parser');
 app.use(express.static(path.join(__dirname,'public')));
 
-const adminRoutes   =    require('./routes/admin');
-const shopRoutes    =    require('./routes/shop');
+const errorController = require('./controllers/errorpage');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes   =      require('./routes/admin');
+const shopRoutes    =       require('./routes/shop');
 const contactRoutes =    require('./routes/contacts');
 const errorpage404  =    require('./controllers/errorpage');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);    
+app.use(shopRoutes);
 app.use(contactRoutes);
-
-
-app.use('/success', (req,res,next) => {
-    res.send('<html><body><script>alert("Form successfuly filled")</script></body></html>')
-})
-
-app.use(errorpage404.errorpage);
+app.use(errorController.errorpage);
 
 app.listen(3000);
