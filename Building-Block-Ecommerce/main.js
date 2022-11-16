c18c76f9658980c013c4cbb45e95ecae9864ea8c
Manish Kumar Pandey <mnish.libra@gmail.com>
2022-11-16 12:16:58 +0530

    Integrating Products

--- Building-Block-Ecommerce/main.js
+++ Building-Block-Ecommerce/main.js
@@ -6,9 +6,8 @@ const parentNode = document.getElementById('music-content');
 
 window.addEventListener('load', () => {
     console.log('loaded');
-
     axios.get('http://localhost:3000/products').then((products) => {
-        console.log(products)
+        console.log(products.data)
         products.data.forEach(product => {
             const productHtml = `
                 <div id="album-${product.id}">
