fbf4da046e3161a15e019b3224e5c6afc401ab61
Manish Pandey <mnish.libra@gmail.com>
2022-12-11 00:20:06 +0530

    Signup

---
+++ Tracking Application/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const sequelize = require('./util/database');
+const user = require('./model/user');
+const port = 3000;
+const routes = require('./route/route');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(routes);
+
+sequelize
+// .sync({force : true})
+.sync()
+.then((response) => {
+    app.listen(port,console.log(`listening at port ${port}...`))
+})
+.catch((error) => {
+    console.log(error)
+})
